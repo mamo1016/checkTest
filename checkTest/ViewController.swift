03dcea0e37b47d94c146ae5530797360bf560fea
taiga <matsuotaiga.miraidenshi@gmail.com>
2019-04-18 15:13:36 +0900

    Initial Commit

---
+++ checkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checkTest
+//
+//  Created by 松尾大雅 on 2019/04/18.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
